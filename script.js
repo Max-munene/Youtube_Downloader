6ac7ed17452c14abe3daba2f3a90e2093a89bc5e
Max-munene <munenemaxwell2@gmail.com>
2023-06-15 23:57:35 +0300

    updates

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-var downloadBtn = document.querySelector('.download-btn')
-var URLinput = document.querySelector('.URL')
+var convertBtn = document.querySelector('.download-button');
+var URLinput = document.querySelector('.URL');
 
-downloadBtn.addEventListener('click',() => {
-    console.log(`URL: ${URLinput.value}`)
-    sendURL(URLinput.value)
-})
+convertBtn.addEventListener('click', () => {
+  console.log(`URL: ${URLinput.value}`);
+  sendURL(URLinput.value);
+});
 
-async function sendURL(URL) {
-    await fetch(
-			`${URLinput}`,
-			{ mode: "no-cors" },
-			{
-				method: "GET",
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Credentials": true,
-				},
-			},
-		)
-			.then((res) => res.json())
-			// console.log(res.json)
-			.then((json) => console.log(json));
-
-// Replace the fetch ith a redirect to the URL to download the video
-    window.location.replace ( `http://localhost:4000/download? URL=${URL}`)
-} 
+function sendURL(URL) {
+  window.location.href = `http://localhost:4000/download?URL=${URL}`;
+}
--- server/index.js
+++ server/index.js
@@ -1,27 +1,15 @@
-const express = require('express')
-const cors = require("cors")
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus:200
-}
-const ytdl = require('ytdl-core')
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
 const app = express();
-
-app.use(cors(corsOptions))
-// Set app to list to port 4000
+app.use(cors());
 app.listen(4000, () => {
-    console.log('Server is Up!: Port 4000')
-})
-
-//Listen for the get request on the /download path
-app.get('/download', (req, res) => {
-    var URL = req.query.URLinput
-    req.json({url:URLinput})
-// console.log(URL)
-
-res.header('Content-Disposition', 'attachment; filename="Myvideo.mp4"');
-
-ytdl(URLinput, { format: 'mp4' }).pipe(res);
-console.log()
+  console.log("Server Works !!! At port 4000");
+});
+app.get("/download", (req, res) => {
+  var URL = req.query.URL;
+  res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+  ytdl(URL, {
+    format: "mp4",
+  }).pipe(res);
 });
