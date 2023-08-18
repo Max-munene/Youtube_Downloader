cd3c2778182b4b736e54a17308a244ae439e2d6d
Max-munene <munenemaxwell2@gmail.com>
2023-08-18 14:31:27 +0300

    server changes

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-var convertBtn = document.querySelector('.download-button');
-var URLinput = document.querySelector('.URL');
+var convertBtn = document.querySelector('button');
+var URLinput = document.querySelector('.URL-input');
+console.log('convertBtn:', convertBtn);
+console.log('URLinput:', URLinput);
 
-convertBtn.addEventListener('click', () => {
-  console.log(`URL: ${URLinput.value}`);
-  sendURL(URLinput.value);
+convertBtn.addEventListener('click', function () {
+	console.log(`URLinput: ${URLinput.value}`);
+
+	sendURL(URLinput.value);
 });
 
 function sendURL(URL) {
-  window.location.href = `http://localhost:4000/download?URL=${URL}`;
+	window.location.href = `http://localhost:4000/download?URL=${URL}`;
 }
+
+// const express = require('express');
+// const cors = require('cors');
+// const ytdl = require('ytdl-core');
+// const app = express();
+// app.use('/static', express.static('./static'));
+// app.listen(3000, () => {
+// 	console.log('It Works!');
+// });
+
+// app.get('/', (req, res) => {
+// 	res.sendFile('index.html', { root: './' });
+// });
+
+// app.get('/download', (req, res) => {
+// 	var url = req.query.url;
+// 	res.header('Content-Disposition', 'attachment;  filename="Video.mp4');
+// 	ytdl(url, { format: 'mp4' }).pipe(res);
+// });
